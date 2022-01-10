3101869df89a65fb2f3fde2acbe57d805e408dce
SuJin Park <psjin230@gmail.com>
2022-01-10 20:18:16 +0900

    first commit

---
+++ Chapter01/sec03.py
@@ -0,0 +1,8 @@
+#이것은 주석입니다.
+
+print("Hello Python!!")
+print()
+
+print(10,20,30)
+print("Hi! ","My name"," is ","SuJin")
+print()
---
+++ Chapter02/sec01.py
@@ -0,0 +1,41 @@
+#큰따옴표, 작은따옴표 이용
+print("안녕하세요\n")
+print('안녕하세요\n')
+print('"SuJin"입니다.')
+print()
+
+#이스케이프 문자 사용
+print("이름\t나이\t지역")
+print("Sujin\23\t안알려줌")
+print()
+
+#여러 줄 문자열
+print("동해물과 백두산이 마르고 닳도록\n하느님이 보우하사 우리나라 만세\n")
+print("""동해물과 백두산이 마르고 닳도록
+하느님이 보우하사 우리나라 만세
+무궁화 삼천리 화려강산 대한사람
+대한으로 길이 보전하세""")
+print()
+
+#문자 인덱스
+print("문자 선택 연산자")
+print("Hello"[0])
+print("Hello"[1])
+print("Hello"[2])
+print("Hello"[3])
+print("Hello"[4])
+print()
+print("거꾸로")
+print("Hello"[-1])
+print("Hello"[-2])
+print("Hello"[-3])
+print("Hello"[-4])
+print("Hello"[-5])
+print()
+print("안녕하세요"[1:4])
+print("안녕하세요"[2:3])
+print()
+
+#문자열 길이구하기
+print(len("Hello"))
+print()
